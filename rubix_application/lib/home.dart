615c0d06ffb328c4a1bab4061243498d891f6e35
Anish <engineeranish10000@gmail.com>
2024-01-17 16:09:26 +0530

    added login and signup

--- rubix_application/lib/home.dart
+++ rubix_application/lib/home.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:rubix_application/consts/colors.dart';
+import 'package:rubix_application/consts/strings.dart';
+import 'package:rubix_application/consts/styles.dart';
+import 'package:rubix_application/controller/auth_controller.dart';
+import 'package:rubix_application/views/auth_screen/login_screen.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,8 +17,21 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    var controller1 = Get.put(AuthController());
+    return Scaffold(
       backgroundColor: Colors.amber,
+      body: Center(child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                                side: const BorderSide(
+                              color: whiteColor,
+                            )),
+                            onPressed: () async {
+                              await controller1.signoutMethod(context);
+                              Get.offAll(() => const LoginScreen());
+                            },
+                            child:
+                                logout.text.white.fontFamily(semibold).make(),
+                          )),
     );
   }
 }
--- rubix_application/lib/main.dart
+++ rubix_application/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:rubix_application/home.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:rubix_application/consts/colors.dart';
+import 'package:rubix_application/views/auth_screen/login_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,15 +16,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Home(),
+          scaffoldBackgroundColor: Colors.transparent,
+          appBarTheme: const AppBarTheme(
+            // set the bar
+            iconTheme: IconThemeData(
+              color: darkFontGrey,
+            ),
+            elevation: 0.0,
+            backgroundColor: Colors.transparent,
+          ),
+        ),
+      home: const LoginScreen(),
     );
   }
 }
