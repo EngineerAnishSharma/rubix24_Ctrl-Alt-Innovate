aac34f1520c754b50bca0e78471e0740186712ec
Anish <engineeranish10000@gmail.com>
2024-01-17 16:27:43 +0530

    chnages

--- rubix_application/lib/main.dart
+++ rubix_application/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:rubix_application/consts/colors.dart';
-import 'package:rubix_application/views/auth_screen/login_screen.dart';
+import 'package:rubix_application/views/splash_screen/splash_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.transparent,
           ),
         ),
-      home: const LoginScreen(),
+      home: const SplashScreen(),
     );
   }
 }
