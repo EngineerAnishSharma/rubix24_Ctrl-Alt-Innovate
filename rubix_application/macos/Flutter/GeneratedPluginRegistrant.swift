c145e2e068d4868d0b340f2a089da144e2bbc5ca
Ayush <ayush.up105@gmail.com>
2024-01-17 18:19:18 +0530

    loc+stt+tts+localstorage

--- rubix_application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ rubix_application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,14 +9,18 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
+import flutter_tts
 import path_provider_foundation
 import shared_preferences_foundation
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
