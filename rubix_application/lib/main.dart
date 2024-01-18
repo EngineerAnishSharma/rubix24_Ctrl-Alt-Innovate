3782ece135d2085be0e0f2c34906a005d35a0229
Anish <engineeranish10000@gmail.com>
2024-01-18 12:49:08 +0530

    chnages

--- rubix_application/lib/main.dart
+++ rubix_application/lib/main.dart
@@ -1,53 +1,53 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-// import 'package:rubix_application/consts/colors.dart';
-// import 'package:rubix_application/views/onboarding_screen/onboarding.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:get/get_navigation/src/root/get_material_app.dart';
+// // import 'package:rubix_application/consts/colors.dart';
+// // import 'package:rubix_application/views/onboarding_screen/onboarding.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyBg-7h5f7toS-IvD7MQRzbHvd7jDi4KOOM",
-            authDomain: "tsec-hackathon-61291.firebaseapp.com",
-            projectId: "tsec-hackathon-61291",
-            storageBucket: "tsec-hackathon-61291.appspot.com",
-            messagingSenderId: "543797904930",
-            appId: "1:543797904930:web:7c87f73813e2b87ea4ac7b",
-            measurementId: "G-FJR687FJPR"));
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setPreferredOrientations(
+//       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+//   if (kIsWeb) {
+//     await Firebase.initializeApp(
+//         options: const FirebaseOptions(
+//             apiKey: "AIzaSyBg-7h5f7toS-IvD7MQRzbHvd7jDi4KOOM",
+//             authDomain: "tsec-hackathon-61291.firebaseapp.com",
+//             projectId: "tsec-hackathon-61291",
+//             storageBucket: "tsec-hackathon-61291.appspot.com",
+//             messagingSenderId: "543797904930",
+//             appId: "1:543797904930:web:7c87f73813e2b87ea4ac7b",
+//             measurementId: "G-FJR687FJPR"));
             
-  } else {
-    await Firebase.initializeApp();
-  }
-  runApp(const MyApp());
-}
+//   } else {
+//     await Firebase.initializeApp();
+//   }
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.transparent,
-        appBarTheme: const AppBarTheme(
-          // set the bar
-          iconTheme: IconThemeData(
-            color: darkFontGrey,
-          ),
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-        ),
-      ),
-      home: const Onboarding(),
-    );
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         scaffoldBackgroundColor: Colors.transparent,
+//         appBarTheme: const AppBarTheme(
+//           // set the bar
+//           iconTheme: IconThemeData(
+//             color: darkFontGrey,
+//           ),
+//           elevation: 0.0,
+//           backgroundColor: Colors.transparent,
+//         ),
+//       ),
+//       // home: const Onboarding(),
+//     );
+//   }
+// }
